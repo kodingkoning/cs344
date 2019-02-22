4eaa6d2da91444d193bed714eb8c54f903bdb679
Elizabeth Koning <koningelizabeth@gmail.com>
2019-02-21 21:36:24 -0500

    Lab04 Exercise 1

--- lab04/lab_1.py
+++ lab04/lab_1.py
@@ -17,8 +17,36 @@ Therefore, the distribution is as the code outputs, with False: 0.4, True: 0.6
 b.
 i. P(Cavity|catch) = P(Cavity ^ catch) / P(catch) = (0.108+0.072) / (0.108+0.016+0.072+0.144) = 0.529
 So the distribution is: True: 0.529, False: 0.471
-ii. code below
+ii. code below, which agrees with my hand-calculated version
 """
 
+# The Joint Probability Distribution Fig. 13.3 (from AIMA Python)
+P = JointProbDist(['Toothache', 'Cavity', 'Catch'])
+T, F = True, False
+P[T, T, T] = 0.108; P[T, T, F] = 0.012
+P[F, T, T] = 0.072; P[F, T, F] = 0.008
+P[T, F, T] = 0.016; P[T, F, F] = 0.064
+P[F, F, T] = 0.144; P[F, F, F] = 0.576
 
+# Compute P(Cavity|Toothache=T)  (see the text, page 493).
+PC = enumerate_joint_ask('Cavity', {'Catch': T}, P)
+print(PC.show_approx())
 
+"""
+1 (cont)
+c.
+"""
+P_coins = JointProbDist(["coin1", "coin2"])
+H, T = True, False
+P_coins[H, T] = 1/4;
+P_coins[T, T] = 1/4;
+P_coins[T, H] = 1/4;
+P_coins[H, H] = 1/4;
+
+PH = enumerate_joint_ask('coin2', {'coin1': H}, P_coins)
+print(PH.show_approx())
+
+"""
+1c (cont)
+This answer does confirm what I already knew about flipping coins. I also know see why the full joint is generally not used. This means a lot of extra work for a very straightforward problem, at least in this case.
+"""
